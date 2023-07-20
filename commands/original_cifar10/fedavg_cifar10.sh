d68ef7d3d2a0e2931eb056f831bbd9f47787a1a5
Haokun Chen <chenhaokun24549@gmail.com>
2023-07-20 10:27:40 +0200

    s

--- commands/original_cifar10/fedavg_cifar10.sh
+++ commands/original_cifar10/fedavg_cifar10.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=2 python3 main.py \
     --exp_name FedAvg_CIFAR10_CNN_IID --seed 42 --device cuda \
     --dataset TinyImageNet \
     --split_type iid --test_fraction 0 \
-    --model_name ResNet --resize 64 --randhf 0.5 --randjit 0.5 --hidden_size 32 \
+    --model_name ResNet18 --resize 64 --randhf 0.5 --randjit 0.5 --hidden_size 32 \
     --algorithm fedavg --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics acc1 acc5 \
     --K 100 --R 5000 --E 5 --C 0.1 --B 50 --beta 0 \
     --optimizer SGD --lr 0.17 --lr_decay 0.99 --lr_decay_step 1 --criterion CrossEntropyLoss
